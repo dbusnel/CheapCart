30964ef010a22f4c07a8fb1f2c740e4de81d0f4b
David Busnel <dbusnel@Davids-Laptop.local>
2025-02-17 22:09:44 -0500

    can now add items to box and remove them by clicking X

--- src/app/components/GroceriesContainer.js
+++ src/app/components/GroceriesContainer.js
@@ -9,34 +9,58 @@ import '../style/container-styles.css';
  */
 export default function GroceriesContainer() {
     const [cItems, ChangeItems] =  React.useState([]);
+
+    const addItem = (value) => {
+        if (!cItems.includes(value)) {
+            ChangeItems(cItems.concat(value));
+        }
+        console.log(cItems);
+    };
+
+    const removeItem = (value) => {
+        ChangeItems(cItems.filter((item) => item != value));
+        console.log(cItems);
+    }
+
     return (
         <div className="container-border">
-            <CurrentItemsListPart />
-            <ContainerTextInputPart placeholderText="Enter an item" />
+            <CurrentItemsListPart items={cItems} RemoveItem={removeItem}/>
+            <ContainerTextInputPart placeholderText="Enter an item" addItemFunction={addItem} />
         </div>);
 }
-
-
 
 /**
  * PROPS:
  *  placeholderText: The text that will be displayed in the input field.
+ *  addItemFunction: The function that will be called when the user presses 'Enter' in the input field.
  */
 function ContainerTextInputPart(props) 
 {
+    const handleKeyDown = (event) => {
+        if (event.key == 'Enter') {
+            props.addItemFunction(event.target.value);
+        }
+    }
+
     return (
         <div className="container-input-box">
-            <input className="container-input-box" placeholder={props.placeholderText} />
+            <input className="container-input-box" placeholder={props.placeholderText} onKeyDown={handleKeyDown}/>
         </div>
     );
 }
 
+/**
+ * 
+ * PROPS:
+ *  - Items: the items to display in the container
+ *  - RemoveItem: the function to remove an item from the list.
+ */
 function CurrentItemsListPart(props) 
 {
     return (
-    <div className="container-items-list-box">
-        <SingleGroceryItem itemName="Apples" RemoveItem={() => console.log("Removing Apples")} />
-    </div>);
+        <div className="container-items-list-box">
+            {props.items != null && props.items.map((item) => <SingleGroceryItem itemName={item} RemoveItem={props.RemoveItem} key={item}/>)}
+        </div>);
 }
 
 /**
@@ -50,7 +74,7 @@ function SingleGroceryItem(props)
     return (
         <div className='single-item-container'>
             <p className='single-item-name'>{props.itemName}</p>
-            <p className='single-item-close-button' onClick={props.RemoveItem(props.itemName)}>✕</p>
+            <p className='single-item-close-button' onClick={() => props.RemoveItem(props.itemName)}>✕</p>
         </div>
     );
 }
