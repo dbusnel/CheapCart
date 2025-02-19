ddba0a5368dc81786ec1d7ce711a4357d3ed4e6e
David Busnel <dbusnel@Davids-Laptop.local>
2025-02-19 16:48:38 -0500

    Converted store locator to JavaScript

--- src/app/api/locate-stores/find-stores.js
+++ src/app/api/locate-stores/find-stores.js
@@ -2,22 +2,35 @@
 const url = "https://us1.locationiq.com/v1/nearby"
 const API_KEY = "pk.e01c10885217ca8ee2ba000e54ac5478"
 
-export async function GET(lat, lon, radius) {
+export async function GetStores (lat, lon, radius) {
+    
     var params = {
-        'key': API_KEY,
-        'lat': str(lat),
-        'lon': str(lon),
+        'lat': String(lat),
+        'lon': String(lon),
         'tag': 'supermarket',
-        'radius': int(radius),
+        'radius': parseInt(radius),
         'format': 'json'
     };
     
-    const response = await fetch(url, {
-        method: "GET",
-        headers: params
-        });
-
-    console.log(response.json());
+    const response = await fetch(BuildURL(params), {method: "GET"});
+    
+    console.log(BuildURL(params))
+    console.log(await response.json());
+    //console.log(response.json());
 }
 
-//GetNearbyStores('42.358990', '-71.058632', 1000) // Stores within 1 km of Boston
+function BuildURL (params) 
+{
+    let output = url;
+    output += "?key=" + API_KEY;
+    output += "&lat=" + params.lat;
+    output += "&lon=" + params.lon;
+    output += "&lat=" + params.lat;
+    output += "&tag=" + params.tag;
+    output += "&radius=" + params.radius;
+    output += "&format=" + params.format;
+
+    return output;
+}
+
+//GET('42.358990', '-71.058632', 1000) // Stores within 1 km of Boston
